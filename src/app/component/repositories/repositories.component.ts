a71cb227f1074d8eefaf5cda444f19dd800edc38
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-17 21:58:03 +0300

    add template to the repo

--- src/app/component/repositories/repositories.component.ts
+++ src/app/component/repositories/repositories.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Users } from 'src/app/user';
 import { RepoService } from 'src/app/services/repo.service';
 import { Repositories } from 'src/app/repo';
+
 @Component({
   selector: 'app-repositories',
   templateUrl: './repositories.component.html',
