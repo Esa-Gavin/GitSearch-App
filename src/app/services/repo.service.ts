d729070b588c7ede7806960d4a2067794d90d05d
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-17 21:36:58 +0300

    add httpclient in the constructor

--- src/app/services/repo.service.ts
+++ src/app/services/repo.service.ts
@@ -8,5 +8,5 @@ export class RepoService {
   _URL = 'https://api.github.com/users/';
   token = '?8b9e17143d094a525a75e0d0637d4f7691770cb4';
 
-  constructor() {}
+  constructor(public http: HttpClient) {}
 }
