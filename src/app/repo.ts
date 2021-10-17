ebfb09f998e762f21aef026c79640e4f370faede
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-17 21:15:05 +0300

    add content to my classes

---
+++ src/app/repo.ts
@@ -0,0 +1,10 @@
+export class Repositories {
+    constructor(
+      public name: any,
+      public description: any,
+      public watchers: any,
+      public language: any,
+      public forks: any
+    ) {}
+  }
+  
