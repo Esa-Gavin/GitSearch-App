be81b90671cbbd63d2f3118b69474a25c3551cdf
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-17 21:55:45 +0300

    add content to the reo ts

--- src/app/component/repositories/repositories.component.ts
+++ src/app/component/repositories/repositories.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Users } from 'src/app/user';
+import { RepoService } from 'src/app/services/repo.service';
+import { Repositories } from 'src/app/repo';
 @Component({
   selector: 'app-repositories',
   templateUrl: './repositories.component.html',
   styleUrls: ['./repositories.component.css']
 })
 export class RepositoriesComponent implements OnInit {
-
-  constructor() { }
+  repo: Repositories[];
+  constructor(public repoService: RepoService) { }
 
   ngOnInit(): void {
   }
