b19100cf5d8a4018616479cd6b6bd58306ac4413
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-17 21:39:51 +0300

    add output decorator and also an eventemitter

--- src/app/component/search/search.component.ts
+++ src/app/component/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
   value: string;
-
+  @Output() valueEmitter = new EventEmitter<any>();
+  
   constructor() {}
 
   ngOnInit(): void {}
