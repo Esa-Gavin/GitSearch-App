219975a5924105e29eee40d7922137bcfdac8c1f
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-17 21:36:21 +0300

    add my token and also import the necessary imports

--- src/app/services/repo.service.ts
+++ src/app/services/repo.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RepoService {
+  _URL = 'https://api.github.com/users/';
+  token = '?8b9e17143d094a525a75e0d0637d4f7691770cb4';
 
-  constructor() { }
+  constructor() {}
 }
