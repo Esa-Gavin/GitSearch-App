14de7aa71c8ef10a18f791967be37edd884cb9eb
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-17 21:12:32 +0300

    add all my components that i will use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './component/home/home.component';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { RepositoriesComponent } from './component/repositories/repositories.component';
+import { SearchComponent } from './component/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    NavBarComponent,
+    RepositoriesComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
