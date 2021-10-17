a24599825c9f19b11071d60cd0cecdf485d611ad
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-10-17 21:33:33 +0300

    add content in the user service and also home component ts files

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,15 +1,30 @@
+import { UserService } from './../../services/user.service';
+
+import { Users } from 'src/app/user';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  mtumiaji: Users[];
+  tafutaMtumiaji(value) {
+    this.userService.tafutaMtumiaji(value).then(
+      (success) => {
+        this.mtumiaji = this.userService.mtumiaji;
+        console.log(this.mtumiaji);
+      },
+      (error) => {
+        console.log(this.mtumiaji);
+      }
+    );
   }
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.tafutaMtumiaji('Gavin-Kariuki');
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Users } from '../user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  mtumiaji: Users[] = [];
+  _URL = 'https://api.github.com/users/';
+  token = '?8b9e17143d094a525a75e0d0637d4f7691770cb4';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  tafutaMtumiaji(value: string) {
+    interface ApiResponse {
+      avatar_url: string;
+      name: string;
+      bio: string;
+      followers: string;
+      following: string;
+      public_repos: string;
+    }
+    let ahadi = new Promise<void>((resolve, reject) => {
+      this.mtumiaji = [];
+      this.http
+        .get<ApiResponse>(this._URL + value + this.token)
+        .toPromise()
+        .then(
+          (results) => {
+            this.mtumiaji.push(results);
+            console.log(results);
+
+            resolve();
+          },
+          (err) => {
+            reject();
+          }
+        );
+    });
+    return ahadi;
+  }
 }
